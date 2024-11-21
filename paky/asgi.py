31955c8efb129eebd603f887555c6219e2652690
leo <fmleonardo@protonmail.com>
2024-11-21 19:08:57 -0300

    asdadsadsa

--- paky/asgi.py
+++ paky/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for tinderpet project.
+ASGI config for paky project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tinderpet.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "paky.settings")
 
 application = get_asgi_application()
--- paky/urls.py
+++ paky/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for tinderpet project.
+URL configuration for paky project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
